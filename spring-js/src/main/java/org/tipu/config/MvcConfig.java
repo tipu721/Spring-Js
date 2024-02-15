ed8fcc51fcd831c3d9d44129c96202e015b753eb
Tipu <tipu@divine-it.net>
2024-02-15 12:09:06 +0600

    java based config

---
+++ spring-js/src/main/java/org/tipu/config/MvcConfig.java
@@ -0,0 +1,10 @@
+package org.tipu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan({ "org.tipu.controller" })
+public class MvcConfig extends WebMvcConfigurerAdapter {
+}
