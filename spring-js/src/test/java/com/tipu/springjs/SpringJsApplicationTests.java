57aacf02e294a106cda497f1c268794d0f5cfa37
Tipu <tipu@divine-it.net>
2024-02-15 14:50:49 +0600

    sprin mvc with spring boot

---
+++ spring-js/src/test/java/com/tipu/springjs/SpringJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipu.springjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
